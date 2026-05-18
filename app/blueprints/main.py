8968b53d88a7866ced4e502de8e26e55f5c6224e
albert <beyondalbert@gmail.com>
2026-05-18 12:16:31 +0800

    优化首页显示

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -2,8 +2,6 @@
 from flask import Blueprint, redirect, render_template, url_for
 from flask_login import current_user
 
-from ..services import kb_service
-
 bp = Blueprint("main", __name__)
 
 
@@ -11,5 +9,4 @@ bp = Blueprint("main", __name__)
 def index():
     if current_user.is_authenticated:
         return redirect(url_for("user.dashboard"))
-    public_kbs = kb_service.list_public_kbs().limit(8).all()
-    return render_template("main/landing.html", public_kbs=public_kbs)
+    return render_template("main/landing.html")
